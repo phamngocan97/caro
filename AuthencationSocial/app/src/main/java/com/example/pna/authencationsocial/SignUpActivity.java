7d76ecb95edead4f3655a875c1f8598dfc6db675
z <phamngocan97123@gmail.com>
2019-01-08 13:55:41 +0700

    fix version firebase

--- AuthencationSocial/app/src/main/java/com/example/pna/authencationsocial/SignUpActivity.java
+++ AuthencationSocial/app/src/main/java/com/example/pna/authencationsocial/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -48,6 +49,7 @@ public class SignUpActivity extends AppCompatActivity {
                                 startActivity(new Intent(SignUpActivity.this,MainActivity.class));
                                 finish();
                             }else{
+                                Log.d("AAA","signup error: " + task.getException());
                                 Toast.makeText(SignUpActivity.this, "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
                             }
 
